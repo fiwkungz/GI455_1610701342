395a4bd874b8743a8a5d8d63e1fb7ba5e6b9841e
DESKTOP-SNVMSNO\Fiwku <fiwkung74@gmail.com>
2021-01-29 16:20:27 +0700

    Send Assignment Week1

---
+++ GI455_Project/Assets/TextMatching/TextMatchingScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextMatchingScript : MonoBehaviour
+{
+    public Button searchText;
+    public InputField Txtinput;
+    public Text resalt;  
+    public string[] Text = new string[5];
+
+    public void search_btn()
+    {
+        if (Text[0] == Txtinput.text)
+        {
+            resalt.text = Txtinput.text + " Is Found ";
+        }
+        else if (Text[1] == Txtinput.text)
+        {
+            resalt.text = Txtinput.text + " Is Found ";
+        }
+        else if (Text[2] == Txtinput.text)
+        {
+            resalt.text = Txtinput.text + " Is Found ";
+        }
+        else if (Text[3] == Txtinput.text)
+        {
+            resalt.text = Txtinput.text + " Is Found ";
+        }
+        else if (Text[4] == Txtinput.text)
+        {
+            resalt.text = Txtinput.text + " Is Found ";
+        }
+        else
+        {
+            resalt.text = Txtinput.text + " Is Not Found ";
+        }
+    }
+}
